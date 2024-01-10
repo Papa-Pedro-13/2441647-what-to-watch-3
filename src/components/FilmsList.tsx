6841c420d9dd049f4e832687c3fde1f151397182
Papa-Pedro-13 <62937441+Papa-Pedro-13@users.noreply.github.com>
2024-01-10 19:04:21 +0300

    final

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -8,6 +8,7 @@ interface FilmsListProps {
   filmsList: FilmType[];
   onClickHandler: (item: FilmType) => void;
 }
+///final
 const FilmsList: React.FC<FilmsListProps> = ({ filmsList, onClickHandler }) => {
   const [maxFilms, setMaxFilms] = useState(8);
   useEffect(() => {
